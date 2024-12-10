f585384be417d751af4f3aa5d78fe51d7508361e
brianwong0522 <bwong03@syr.edu>
2024-12-10 15:36:03 -0500

    added files

---
+++ RegisterServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.mindrot.jbcrypt.BCrypt;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+
+        // Code to save username and hashedPassword to the database
+    }
+}
